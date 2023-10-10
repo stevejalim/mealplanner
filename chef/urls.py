1c851f9c1b5e9cdc4d913216583b868ced6e6c8a
clarapenn <49952437+clarapenn@users.noreply.github.com>
2023-10-10 13:44:19 +0100

    Creates dish and meal list/create/update views and templates
    Plus url config

--- chef/urls.py
+++ chef/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.dish_list, name="dish_list"),
+    path("", views.show_schedule, name="show_schedule"),
+    path("dish/", views.dish_list, name="dish-list"),
+    path("dish/add/", views.DishCreate.as_view, name="dish-add"),
+    path("dish/update/<int:pk>/", views.DishUpdate.as_view(), name="dish-update"),
+    path("meal/update/<int:pk>/", views.MealUpdate.as_view(), name="meal-update"),
 ]
--- chef/views.py
+++ chef/views.py
@@ -1,3 +1,78 @@
 from django.shortcuts import render
+from .models import Dish, Meal
+from django.utils import timezone
+from django.views.generic import (
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+
 
 # Create your views here.
+
+
+def show_schedule(request):
+
+    date_now = timezone.now().date()
+    meals = Meal.objects.filter(date__gte=date_now).order_by("date")
+
+    return render(request, "chef/meal_list.html", {"meals": meals})
+
+
+def dish_list(request):
+    dishes = Dish.objects.all().order_by("title")
+    return render(request, "chef/dish_list.html", {"dishes": dishes})
+
+
+class DishCreate(CreateView):
+
+    model = Dish
+    fields = [
+        "title",
+        "text",
+    ]
+
+    def get_context_data(self):
+
+        context = super().get_context_data()
+        context["title"] = "Create a new dish"
+
+        return context
+
+
+class DishUpdate(UpdateView):
+
+    model = Dish
+    fields = [
+        "title",
+        "text",
+    ]
+
+    def get_context_data(self):
+
+        context = super().get_context_data()
+        context["title"] = "Edit dish"
+        return context
+
+    def get_success_url(self):
+
+        return reverse("dish-list")
+
+
+class MealUpdate(UpdateView):
+
+    model = Meal
+    fields = [
+        "dish",
+        "date",
+    ]
+
+    def get_context_data(self):
+
+        context = super().get_context_data()
+        context["title"] = "Edit meal"
+        return context
+
+    def get_success_url(self):
+
+        return reverse("show-schedule")
