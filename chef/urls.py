3ccaed94b51f5fa26b5dcf4fb2c722864453110e
clarapenn <49952437+clarapenn@users.noreply.github.com>
2023-10-19 15:04:42 +0100

    Add login required to view lists of meals and dishes
    Adds it to the urls instead of directly on the views

--- chef/urls.py
+++ chef/urls.py
@@ -1,15 +1,32 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from . import views
 
 
 urlpatterns = [
     path("", views.meal_schedule, name="meal-schedule"),
-    path("dishes/", views.dish_list, name="dish-list"),
-    path("dish/add/", views.DishCreate.as_view(), name="dish-add"),
-    path("dish/update/<int:pk>/", views.DishUpdate.as_view(), name="dish-update"),
-    path("dish/delete/<int:pk>/", views.DishDelete.as_view(), name="dish-delete"),
-    path("meals/", views.meal_list, name="meal-list"),
-    path("meal/add/", views.MealCreate.as_view(), name="meal-add"),
-    path("meal/update/<int:pk>/", views.MealUpdate.as_view(), name="meal-update"),
-    path("meal/delete/<int:pk>/", views.MealDelete.as_view(), name="meal-delete"),
+    path("dishes/", login_required(views.dish_list), name="dish-list"),
+    path("dish/add/", login_required(views.DishCreate.as_view()), name="dish-add"),
+    path(
+        "dish/update/<int:pk>/",
+        login_required(views.DishUpdate.as_view()),
+        name="dish-update",
+    ),
+    path(
+        "dish/delete/<int:pk>/",
+        login_required(views.DishDelete.as_view()),
+        name="dish-delete",
+    ),
+    path("meals/", login_required(views.meal_list), name="meal-list"),
+    path("meal/add/", login_required(views.MealCreate.as_view()), name="meal-add"),
+    path(
+        "meal/update/<int:pk>/",
+        login_required(views.MealUpdate.as_view()),
+        name="meal-update",
+    ),
+    path(
+        "meal/delete/<int:pk>/",
+        login_required(views.MealDelete.as_view()),
+        name="meal-delete",
+    ),
 ]
