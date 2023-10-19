b219475666980d04f63777d4753371ab4a01a49b
clarapenn <49952437+clarapenn@users.noreply.github.com>
2023-10-19 15:00:58 +0100

    Create user foreign key fields for Meal and Dish models

--- chef/models.py
+++ chef/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Dish(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
+
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=True,
+    )
 
     def __str__(self):
         return self.title
@@ -12,6 +19,12 @@ class Dish(models.Model):
 class Meal(models.Model):
     dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
     date = models.DateField()
+
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=True,
+    )
 
     def __str__(self):
         return f"{self.dish} on {self.date}"
