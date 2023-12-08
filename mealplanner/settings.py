e74f00f53cde2cdd04031c2b7a00e6f8e9b9012a
clarapenn <49952437+clarapenn@users.noreply.github.com>
2023-12-08 11:00:25 +0000

    Move CSS to separate file and provide way to add class to body tag

--- mealplanner/settings.py
+++ mealplanner/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEALPLANNER_PROJECT_DIR = Path(__file__).resolve().parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -119,7 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = MEALPLANNER_PROJECT_DIR / "static_collected"
+STATICFILES_DIRS = [
+    BASE_DIR / "chef" / "static",
+]
+# same as:
+# STATIC_ROOT = BASE_DIR / "mealplanner" / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
