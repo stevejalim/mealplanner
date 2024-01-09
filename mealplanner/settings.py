8d7ae9f314bc8c80b6805af1c3825d45eea92185
clarapenn <49952437+clarapenn@users.noreply.github.com>
2024-01-09 12:01:04 +0000

    Update config ready for PythonAnywhere

--- mealplanner/settings.py
+++ mealplanner/settings.py
@@ -19,15 +19,16 @@ MEALPLANNER_PROJECT_DIR = Path(__file__).resolve().parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!d=s)dg+off(e$%u^kn+o2l=7xifd1b3sd*l1#vsk0*(^blv@q"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", True)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    "cleop.eu.pythonanywhere.com",
+    "localhost",
+]
 
 # Application definition
 
